5a2fa74dc02a0fe4fb5e6f3f56278b892a360733
gogglesbdj <pucklechurch17@gmail.com>
2025-02-21 00:13:36 -0400

    feat: update page title and favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import './Header.css';
 const Header: React.FC = () => (
     <header className='header'>
         <img src='/logo.png' alt='logo' className='logo' />
+        <h1 className='title'>Movie App</h1>
     </header>
 );
 
